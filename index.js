f48517aac21696024ee66a97cbf4857a73c15fbf
Hanson <hanson.hua903@gmail.com>
2023-07-13 17:18:37 -0500

    Complete

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  const storeNum = {};
+  for(let i = 0; i < array.length; i++){
+    const currNum = array[i];
+    const total = target - currNum;
+    if (storeNum[total]){
+      return true;
+    };
+    storeNum[currNum] = true;
+  };
+  return false;
+};
 
 /* 
   Write the Big O time complexity of your function here
+
+  - The Big O time complexity of hasTargetSum(array, target) is O(n) n is the number of elements in the given array
+
 */
 
 /* 
   Add your pseudocode here
+
+  function hasTargetSum(array,target)
+
+    initialize empty array called storeNum
+
+    iterate over each number in the input array:
+
+      initialize a constant to store the current number
+      calculate the differences of target minus current number
+
+      if the complement exist
+        return true
+
+      complement exist = true
+  
+  if no complement exist
+    return false
+
 */
 
 /*
   Add written explanation of your solution here
+
+  write a function called hasTargetSum taking two arguments array and index target:
+    create empty object to store complements
+
+    iterate over array to see if any complements exist
+      complements = (any number with-in the array = target)
+    
+    return true if complement exist
+  
+  return false if complement does not exist
+
 */
 
 // You can run `node index.js` to view these console logs
